e304b349955f0f5f0a81e182a49817729c305111
Shaurya0987 <shaurya3537@gmail.com>
2025-10-13 22:58:38 +0530

    Mobile Login Added

--- lib/Mobile web/loginmob.dart
+++ lib/Mobile web/loginmob.dart
@@ -1,4 +1,6 @@
+import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:vakilhire/Colors/app_colors.dart';
 
 class Loginmob extends StatelessWidget {
@@ -6,6 +8,43 @@ class Loginmob extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: AppColors.surface);
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      body: Center(
+        child: Padding(
+          padding: EdgeInsetsGeometry.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Welcome Back!",
+                style: GoogleFonts.poppins(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 15),
+              RichText(
+                textAlign: TextAlign.center,
+                text: TextSpan(
+                  style: TextStyle(color: Colors.black, fontSize: 14),
+                  children: [
+                    TextSpan(text: "Boost your confidence with "),
+                    TextSpan(
+                      text: "Legal Hire",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    TextSpan(
+                      text:
+                          " —connect to the right lawyer, get expert guidance, and tackle any legal challenge with ease and assurance.",
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
